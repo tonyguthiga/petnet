9fea93b38ff25a82bebb9c89da106f07b7f98933
tonyguthiga <toniguthiga93@gmail.com>
2023-07-27 09:48:00 +0300

    Show vendor on product detail page

--- petnet/urls.py
+++ petnet/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from core.views import frontpage, about
 
 urlpatterns = [
-    path('', include('store.urls')),
-    path('', frontpage, name='frontpage'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+    path('', frontpage, name='frontpage'),
 ]
  
